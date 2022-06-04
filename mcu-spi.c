d0153a82ce800a9f04e1ad01ebb42beee7c36a69
DoZh <TATQAQTAT@gmail.com>
2022-06-05 01:19:04 +0800

    try to use sysfs to recv msg

--- mcu-spi.c
+++ mcu-spi.c
@@ -22,6 +22,9 @@
 #define PAYLOAD_SHIFT HEAD_LENGTH
 #define MAX_PACKET_LENGTH (HEAD_LENGTH + MAX_PAYLOAD_LENGTH + VERIFY_LENGTH) //1096
 
+#define SEND_SYSFS_DIR_NAME "send"
+#define RECV_SYSFS_DIR_NAME "recv"
+
 #define MAX_BUFFERED_MSG 1024 
 
 /* This structure will represent single device */
@@ -46,28 +49,63 @@ typedef struct mcu_message {
 	uint8_t * payload;
 }mcu_message;
 
-int init_mcu_message_queue(mcu_message_queue *msg_queue)
-{
-	msg_queue = kzalloc(sizeof(mcu_message_queue), GFP_KERNEL);
-	if (!msg_queue) {
-		return -ENOMEM;
+void dev_dump_hex(const void* data, size_t size) {
+	char ascii[17];
+	size_t i, j;
+	ascii[16] = '\0';
+	for (i = 0; i < size; ++i) {
+		printk( KERN_CONT  "%02X ", ((unsigned char*)data)[i]);
+		if (((unsigned char*)data)[i] >= ' ' && ((unsigned char*)data)[i] <= '~') {
+			ascii[i % 16] = ((unsigned char*)data)[i];
+		} else {
+			ascii[i % 16] = '.';
+		}
+		if ((i+1) % 8 == 0 || i+1 == size) {
+			printk( KERN_CONT  " ");
+			if ((i+1) % 16 == 0) {
+				printk( KERN_CONT  "|  %s \n", ascii);
+			} else if (i+1 == size) {
+				ascii[(i+1) % 16] = '\0';
+				if ((i+1) % 16 <= 8) {
+					printk( KERN_CONT " ");
+				}
+				for (j = (i+1) % 16; j < 16; ++j) {
+					printk( KERN_CONT "   ");
+				}
+				printk( KERN_CONT "|  %s \n", ascii);
+			}
+		}
 	}
-	msg_queue->read_msg_idx = 0;
-	msg_queue->write_msg_idx = 0;
-	msg_queue->msg_count = 0;
-	return 0;
+}
+
+bool is_mcu_message_queue_full(mcu_message_queue *msg_queue) 
+{
+	return msg_queue->msg_count >= MAX_BUFFERED_MSG;
+}
+
+bool is_mcu_message_queue_empty(mcu_message_queue *msg_queue) 
+{
+	return msg_queue->msg_count <= 0;
+}
+
+int get_payload_len_in_next_mcu_msg(mcu_message_queue *msg_queue) 
+{
+	return msg_queue->mcu_msg[msg_queue->read_msg_idx]->payload_length;
 }
 
 int store_one_mcu_message(mcu_message_queue *msg_queue, 
 			uint16_t payload_length, uint8_t *payload_desc, uint8_t *payload)
 {
+	printk("store_one_mcu_message\n");
 	if (msg_queue->msg_count >= MAX_BUFFERED_MSG) {
 		return -ENOSPC;
 	}
+
 	mcu_message * mcu_msg = kzalloc(sizeof(mcu_message), GFP_KERNEL);
 	if (!mcu_msg) {
 		return -ENOMEM;
 	}
+	
 	memcpy(mcu_msg->payload_desc, payload_desc, PAYLOAD_DESC_LENGTH);
 	mcu_msg->payload = NULL;
 	if (payload_length > 0) {
@@ -93,6 +131,25 @@ int load_one_mcu_message(mcu_message_queue *msg_queue,
 	if (msg_queue->msg_count <= 0) {
 		return -EAGAIN;
 	}
+
+	//clean up memory of previous msg
+	struct mcu_message *prev_mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
+	if (prev_mcu_msg) {
+		if (prev_mcu_msg->payload_length > 0 && prev_mcu_msg->payload) {
+			//memcpy(payload, mcu_msg->payload, mcu_msg->payload_length);
+			kfree (prev_mcu_msg->payload);
+			prev_mcu_msg->payload = NULL;
+		}
+		//memcpy(payload_desc, mcu_msg->payload_desc, PAYLOAD_DESC_LENGTH);
+		//*payload_length = mcu_msg->payload_length;
+		kfree(prev_mcu_msg);
+		msg_queue->mcu_msg[msg_queue->read_msg_idx] = NULL;
+	}
+
+	(msg_queue->read_msg_idx)++;
+	if ((msg_queue->read_msg_idx) >= MAX_BUFFERED_MSG) {
+		msg_queue->read_msg_idx = 0;
+	}
 	mcu_message * mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
 	if (!mcu_msg) {
 		return -EFAULT; 
@@ -105,14 +162,85 @@ int load_one_mcu_message(mcu_message_queue *msg_queue,
 		}
 		memcpy(payload, mcu_msg->payload, mcu_msg->payload_length);
 		kfree (mcu_msg->payload);
+		mcu_msg->payload = NULL;
 	}
 	kfree(mcu_msg);
 	msg_queue->mcu_msg[msg_queue->read_msg_idx] = NULL;
+
+	(msg_queue->msg_count)--;
+	return 0;
+}
+
+int drop_one_mcu_message(mcu_message_queue *msg_queue)
+{
+	if (msg_queue->msg_count <= 0) {
+		return -EAGAIN;
+	}
+
+	//clean up memory of previous msg
+	struct mcu_message *prev_mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
+	if (prev_mcu_msg) {
+		if (prev_mcu_msg->payload_length > 0 && prev_mcu_msg->payload) {
+			//memcpy(payload, mcu_msg->payload, mcu_msg->payload_length);
+			kfree (prev_mcu_msg->payload);
+			prev_mcu_msg->payload = NULL;
+		}
+		//memcpy(payload_desc, mcu_msg->payload_desc, PAYLOAD_DESC_LENGTH);
+		//*payload_length = mcu_msg->payload_length;
+		kfree(prev_mcu_msg);
+		msg_queue->mcu_msg[msg_queue->read_msg_idx] = NULL;
+	}
+
 	(msg_queue->read_msg_idx)++;
 	if ((msg_queue->read_msg_idx) >= MAX_BUFFERED_MSG) {
 		msg_queue->read_msg_idx = 0;
 	}
+	mcu_message * mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
+	if (!mcu_msg) {
+		return -EFAULT; 
+	}
+	if (mcu_msg->payload_length > 0) {
+		if (!mcu_msg->payload) {
+			return -EFAULT;
+		}
+		kfree (mcu_msg->payload);
+		mcu_msg->payload = NULL;
+	}
+	kfree(mcu_msg);
+	msg_queue->mcu_msg[msg_queue->read_msg_idx] = NULL;
+	
 	(msg_queue->msg_count)--;
+	return 0;
+}
+
+int init_mcu_message_queue(mcu_message_queue **msg_queue)
+{
+	*msg_queue = kzalloc(sizeof(mcu_message_queue), GFP_KERNEL);
+	if (!msg_queue) {
+		return -ENOMEM;
+	}
+	(*msg_queue)->read_msg_idx = MAX_BUFFERED_MSG - 1;
+	(*msg_queue)->write_msg_idx = 0;
+	(*msg_queue)->msg_count = 0;
+	printk("init_mcu_message_queue:%p\n", *msg_queue);
+	return 0;
+}
+
+int deinit_mcu_message_queue(mcu_message_queue *msg_queue)
+{
+	if (!msg_queue) {
+		return -EFAULT;
+	}
+	while (!is_mcu_message_queue_empty(msg_queue)) {
+		drop_one_mcu_message(msg_queue);
+	}
+	kfree(msg_queue);
+	return 0;
+}
+
+
+int parser_string_to_msg (uint8_t *str, uint8_t *payload_desc, uint16_t *count, uint8_t *payload)
+{
 	return 0;
 }
 
@@ -167,6 +295,8 @@ static ssize_t mcuspi_read_file(struct file *file, char __user *userbuf,
 
 	return 0;
 }
+
+
 
 /* Writing from the terminal command line, \n is added */
 static ssize_t mcuspi_write_file(struct file *file, const char __user *userbuf,
@@ -240,7 +370,46 @@ static irqreturn_t mcu_spi_isr(int irq_no, void *data)
 {
 	int val;
 	struct mcuspi_dev * mcuspi = data;
+	uint8_t *buf;
+	int status = 0;
+	int payload_length = 0;
+
+
 	dev_info(&mcuspi->spid->dev, "interrupt received. device: %s\n", mcuspi->name);
+	buf = kzalloc(MAX_PACKET_LENGTH, GFP_KERNEL);
+	if (!buf) {
+		dev_info(&mcuspi->spid->dev, "allocate memory fail in isr. device: %s\n", mcuspi->name);
+		return IRQ_HANDLED;
+	}
+	status = spi_read(mcuspi->spid, buf, MAX_PACKET_LENGTH); 
+	if (status) {
+		dev_info(&mcuspi->spid->dev, "spi read fail in isr. device: %s\n", mcuspi->name);
+		kfree(buf);
+		return IRQ_HANDLED;
+	}
+
+	dev_dump_hex(buf, MAX_PACKET_LENGTH);
+
+	payload_length = *(uint16_t *)(buf + PAYLOAD_SHIFT - 2);
+	payload_length = max(payload_length, 0);
+	payload_length = min(payload_length, MAX_PAYLOAD_LENGTH);
+	
+	dev_info(&mcuspi->spid->dev,
+		 "store_one_mcu_message, payload_length:%d, PREAMBLE_LENGTH + SERIAL_NO_LENGTH:%d, PAYLOAD_SHIFT:%d\n", 
+		 		payload_length, PREAMBLE_LENGTH + SERIAL_NO_LENGTH, PAYLOAD_SHIFT);
+
+	dev_info(&mcuspi->spid->dev,
+		 "store_one_mcu_message, buf:%ld, payload_desc:%ld, payload:%ld\n", 
+		 		buf, buf + PREAMBLE_LENGTH + SERIAL_NO_LENGTH, buf + PAYLOAD_SHIFT);
+
+	printk("mcuspi->msg_queue:%p\n", mcuspi->msg_queue); 
+	status = store_one_mcu_message(mcuspi->msg_queue, 
+			payload_length, buf + PREAMBLE_LENGTH + SERIAL_NO_LENGTH, buf + PAYLOAD_SHIFT);
+	kfree(buf);
+
+	if (status) {
+		dev_info(&mcuspi->spid->dev, "store msg fail in isr. errno:%d device: %s\n", status, mcuspi->name);
+	}
 /*
 	val = gpiod_get_value(priv->gpio);
 	dev_info(priv->dev, "Button state: 0x%08X\n", val);
@@ -259,6 +428,153 @@ static irqreturn_t mcu_spi_isr(int irq_no, void *data)
 	return IRQ_HANDLED;
 }
 
+static ssize_t recv_payload_show(struct device *dev,
+				 struct device_attribute *attr, char *buf)
+{	
+	struct mcuspi_dev * mcuspi;
+	struct spi_device * spid;
+	struct mcu_message_queue * msg_queue;
+	struct mcu_message * mcu_msg;
+
+	spid = to_spi_device(dev);
+	mcuspi = spi_get_drvdata(spid);
+	msg_queue = mcuspi->msg_queue;
+	mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
+	if (!mcu_msg) {
+		return -EFAULT; 
+	}
+	if (mcu_msg->payload_length > 0)
+		memcpy(buf, mcu_msg->payload, mcu_msg->payload_length);
+	return mcu_msg->payload_length;
+}
+static DEVICE_ATTR(payload, S_IRUGO, recv_payload_show, NULL);
+
+
+static ssize_t recv_payload_len_show(struct device *dev,
+				 struct device_attribute *attr, char *buf)
+{
+	struct mcuspi_dev * mcuspi;
+	struct spi_device * spid;
+	struct mcu_message_queue * msg_queue;
+	struct mcu_message * mcu_msg;
+
+	spid = to_spi_device(dev);
+	mcuspi = spi_get_drvdata(spid);
+	msg_queue = mcuspi->msg_queue;
+	mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
+	if (!mcu_msg) {
+		return -EFAULT; 
+	}
+	memcpy(buf, &(mcu_msg->payload_length), sizeof(mcu_msg->payload_length));
+	return sizeof(mcu_msg->payload_length);
+}
+static DEVICE_ATTR(payload_len, S_IRUGO, recv_payload_len_show, NULL);
+
+static ssize_t recv_payload_desc_show(struct device *dev,
+				 struct device_attribute *attr, char *buf)
+{
+	struct mcuspi_dev * mcuspi;
+	struct spi_device * spid;
+	struct mcu_message_queue * msg_queue;
+	struct mcu_message * mcu_msg;
+
+	spid = to_spi_device(dev);
+	mcuspi = spi_get_drvdata(spid);
+	msg_queue = mcuspi->msg_queue;
+	mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
+	if (!mcu_msg) {
+		return -EFAULT; 
+	}
+	memcpy(buf, mcu_msg->payload_desc, PAYLOAD_DESC_LENGTH);
+	return PAYLOAD_DESC_LENGTH;
+}
+static DEVICE_ATTR(payload_desc, S_IRUGO, recv_payload_desc_show, NULL);
+
+static ssize_t recv_remain_msg_count_show(struct device *dev,
+				 struct device_attribute *attr, char *buf)
+{
+	struct mcuspi_dev * mcuspi;
+	struct spi_device * spid;
+	struct mcu_message_queue * msg_queue;
+
+	spid = to_spi_device(dev);
+	mcuspi = spi_get_drvdata(spid);
+	msg_queue = mcuspi->msg_queue;
+
+	dev_info(&mcuspi->spid->dev,
+		 "recv_remain_msg_count_show, spid:%p, mcuspi:%p, msg_queue:%p, msg_cnt:%d\n", 
+	 		spid, mcuspi, msg_queue, msg_queue->msg_count);
+
+	memcpy(buf, &(msg_queue->msg_count), sizeof(msg_queue->msg_count));
+	return sizeof(msg_queue->msg_count);
+}
+static DEVICE_ATTR(remain_msg_count, S_IRUGO, recv_remain_msg_count_show, NULL);
+
+static ssize_t recv_get_msg_store(struct device *dev,
+				 struct device_attribute *attr, const char *buf,
+				 size_t count)
+{
+	struct mcuspi_dev * mcuspi;
+	struct spi_device * spid;
+	struct mcu_message_queue * msg_queue;
+	struct mcu_message * prev_mcu_msg;
+
+	spid = to_spi_device(dev);
+	mcuspi = spi_get_drvdata(spid);
+	msg_queue = mcuspi->msg_queue;
+
+	//clean up memory of previous msg
+	prev_mcu_msg = msg_queue->mcu_msg[msg_queue->read_msg_idx];
+	dev_info(&mcuspi->spid->dev,
+		 "recv_get_msg_store, spid:%p, mcuspi:%p, msg_queue:%p, prev_mcu_msg:%p, read_msg_idx:%d\n", 
+	 		spid, mcuspi, msg_queue, prev_mcu_msg, msg_queue->read_msg_idx);
+	if (prev_mcu_msg) {
+		if (prev_mcu_msg->payload_length > 0 && prev_mcu_msg->payload) {
+			//memcpy(payload, mcu_msg->payload, mcu_msg->payload_length);
+			kfree (prev_mcu_msg->payload);
+			prev_mcu_msg->payload = NULL;
+		}
+		//memcpy(payload_desc, mcu_msg->payload_desc, PAYLOAD_DESC_LENGTH);
+		//*payload_length = mcu_msg->payload_length;
+		kfree(prev_mcu_msg);
+		msg_queue->mcu_msg[msg_queue->read_msg_idx] = NULL;
+	}
+
+
+	if (msg_queue->msg_count <= 0) {
+		return -EAGAIN;
+	}
+	(msg_queue->msg_count)--;
+	(msg_queue->read_msg_idx)++;
+	if ((msg_queue->read_msg_idx) >= MAX_BUFFERED_MSG) {
+		msg_queue->read_msg_idx = 0;
+	}
+	/*
+	
+	*/
+	return count;
+}
+static DEVICE_ATTR(get_msg, S_IWUSR|S_IWGRP, NULL, recv_get_msg_store);
+
+static struct attribute *recv_msg_attributes[] = {
+	&dev_attr_payload.attr,
+	&dev_attr_payload_len.attr,
+	&dev_attr_payload_desc.attr,
+	&dev_attr_remain_msg_count.attr,
+	&dev_attr_get_msg.attr,
+	NULL
+};
+
+static const struct attribute_group recv_msg_attr_group = {
+	.attrs = recv_msg_attributes,
+	.name = RECV_SYSFS_DIR_NAME,
+};
+
+static const struct attribute_group *msg_attr_groups[] = {
+	&recv_msg_attr_group,
+	NULL,
+};
+
 /* declare a file_operations structure */
 static const struct file_operations mcuspi_fops = {
 	.owner = THIS_MODULE,
@@ -268,7 +584,7 @@ static const struct file_operations mcuspi_fops = {
 
 static int mcu_spi_probe(struct spi_device *spid)
 {
-	int ret;
+	int ret = 0;
 	int err = 0;
 
 	static int counter = 0;
@@ -324,9 +640,17 @@ static int mcu_spi_probe(struct spi_device *spid)
 		return ERR_PTR(err);
 
 
-	/* Register misc device */
-	ret =  misc_register(&mcuspi->mcu_spi_miscdevice);
+	/* Register sysfs hooks */
+	ret |= sysfs_create_groups(&spid->dev.kobj, msg_attr_groups);
+	//ret |= sysfs_create_group(&spid->dev.kobj, &recv_msg_attr_group);
+	dev_info(&spid->dev, "spid->dev.kobj: %s", spid->dev.kobj.name);
+	//sysfs file may under /sys/class/spi_master/spix/spix.y/ZZZ
 
+	/* Register misc device */
+	ret |=  misc_register(&mcuspi->mcu_spi_miscdevice);
+
+	ret |= init_mcu_message_queue(&mcuspi->msg_queue);
+	
 	//test crc32
 	uint32_t crc32_result = ~crc32(0xFFFFFFFF, "UUUUUUUUUUUUUUUU", 15);
 	dev_info(&spid->dev, "The crc32 is: %x\n", crc32_result);
@@ -349,8 +673,14 @@ static int mcu_spi_remove(struct spi_device *spid)
 	dev_info(&spid->dev, 
 		 "mcu_spi_remove is entered on %s\n", mcuspi->name);
 
+	deinit_mcu_message_queue(mcuspi->msg_queue);
+	
 	/* Deregister misc device */
 	misc_deregister(&mcuspi->mcu_spi_miscdevice);
+
+	/* Deregister sysfs hooks */
+	sysfs_remove_groups(&spid->dev.kobj, msg_attr_groups);
+	//sysfs_remove_group(&spid->dev.kobj, &recv_msg_attr_group);
 
 	dev_info(&spid->dev, 
 		 "mcu_spi_remove is exited on %s\n", mcuspi->name);
